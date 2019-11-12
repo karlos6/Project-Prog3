581697247f97ea6af84aa2490ff8d3d4a50dc515
karlos6 <Carlos.Keller.M@gmail.com>
2019-11-11 22:32:06 -0500

    version home casi terminado

--- HogarColombia/src/app/app.module.ts
+++ HogarColombia/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { HomeComponent } from './public/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
